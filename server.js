eb4fc952cd3a658130d269827f77e15f83550967
mamta srinivas <mamtasrinivassp@gmail.com>
2020-05-06 11:30:59 +0200

    Setup with mongoose

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
-const monogdb = require('mongodb')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
 const app = express()
 const port = process.env.PORT || 3000
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Database connected:')
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Hello World app listening at http://localhost:${port}`))
+// app.listen(port, () => console.log(`Hello World app listening at http://localhost:${port}`))
